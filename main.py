d19bf64b5f82f8c57b1ca2b5788b6f2ba673a999
kbowater57 <49311506+kbowater57@users.noreply.github.com>
2019-04-05 14:13:14 +0100

    Updated main.py
    This now has the basic functionality required, but no validation for the correct type of input.

--- main.py
+++ main.py
@@ -3,5 +3,10 @@
 Created on Fri Apr  5 14:00:14 2019
 
 @author: kbowa
+This project will produce a text interface for a generator for n-sided dice rolls
 """
+import random
+#print ("Hello there! I am Dicey. How many sides would you like your dice to be?")
+sides = eval(input("Hello there! I am Dicey. How many sides would you like your dice to have? \n"))
+print("Your " + str(sides) + "-sided dice rolled a " + str(random.randrange(1,sides)) + " .")
 
