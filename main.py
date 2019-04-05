4fccd8448b579af8d63a22465e0961dc22c5dec6
kbowater57 <49311506+kbowater57@users.noreply.github.com>
2019-04-05 15:04:11 +0100

    Updated main.py
    Now allows multiple dice rolls, checking for sensible input at each step.

--- main.py
+++ main.py
@@ -3,16 +3,31 @@
 Created on Fri Apr  5 14:00:14 2019
 
 @author: kbowa
-This project will produce a text interface for a generator for n-sided dice rolls
+This project will produce a text interface for a generator for m n-sided dice
+rolls, checking for sensible user input.
 """
 import random
-#print ("Hello there! I am Dicey. How many sides would you like your dice to be?")
-#sides = eval(input("Hello there! I am Dicey. How many sides would you like your dice to have? \n"))
-sides_untested = input("Hello there! I am Dicey. How many sides would you like your dice to have? \n")
-try: 
-    sides = eval(sides_untested)
-    print("Your " + str(sides) + "-sided dice rolled a " + str(random.randrange(1,sides)) + " .")
+
+all_done = 0
+print("Hello there! I am Dicey.")
+while all_done == 0:
     
-except (ValueError,NameError):
-    print("A dice can't have " + sides_untested + " sides!")
-#print("Your " + str(sides) + "-sided dice rolled a " + str(random.randrange(1,sides)) + " .")
+    sides_untested = input("How many sides would you like your dice to have? \n")
+    
+    try: 
+        sides = eval(sides_untested)
+        print("Your " + str(sides) + "-sided dice rolled a " + str(random.randrange(1,sides)) + " .")
+        user_satisfied = str(input("Would you like to roll any more dice? Type y or n.\n"))
+        if user_satisfied == "y":
+            print("Okay! Coming right up.")
+        elif user_satisfied == "n":
+            print("Thank you for using my services!")
+            all_done = 1
+        elif user_satisfied == "n.":
+            print("You are a true coder.Thank you for using my services!")
+            all_done = 1
+        else:
+            print("What kind of an answer is that?!")
+            all_done = 1
+    except (ValueError,NameError):
+        print("A dice can't have " + sides_untested + " sides!")
