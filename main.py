26d80e86d050dc9dec260efa30f6b0f96a5c1dc2
kbowater57 <49311506+kbowater57@users.noreply.github.com>
2019-04-05 14:42:50 +0100

    Updated main.py
    Now does type checking on the user input.

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ This project will produce a text interface for a generator for n-sided dice roll
 """
 import random
 #print ("Hello there! I am Dicey. How many sides would you like your dice to be?")
-sides = eval(input("Hello there! I am Dicey. How many sides would you like your dice to have? \n"))
-print("Your " + str(sides) + "-sided dice rolled a " + str(random.randrange(1,sides)) + " .")
-
+#sides = eval(input("Hello there! I am Dicey. How many sides would you like your dice to have? \n"))
+sides_untested = input("Hello there! I am Dicey. How many sides would you like your dice to have? \n")
+try: 
+    sides = eval(sides_untested)
+    print("Your " + str(sides) + "-sided dice rolled a " + str(random.randrange(1,sides)) + " .")
+    
+except (ValueError,NameError):
+    print("A dice can't have " + sides_untested + " sides!")
+#print("Your " + str(sides) + "-sided dice rolled a " + str(random.randrange(1,sides)) + " .")
